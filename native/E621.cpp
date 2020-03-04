b4d72d42a33ecd1c9ffaf0c28513d03292329458
Manuel Stör <manuel.stoer@outlook.com>
2020-03-04 19:58:41 +0100

    Cleanup native code a bit

--- native/E621.cpp
+++ native/E621.cpp
@@ -1,67 +1,28 @@
-#include <Godot.hpp>
-#include <Reference.hpp>
+#include "E621.hpp"
 
 using namespace godot;
 
-class E621 : public Reference
+void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o)
 {
-    GODOT_CLASS(E621, Reference);
+    godot::Godot::gdnative_init(o);
+}
 
-public:
-    E621() {}
-
-    void _init() {
-        test_void_method();
-    }
-    
-    void test_void_method()
-    {
-        Godot::print("This is a test!");
-    }
-
-    Variant method(Variant arg)
-    {
-        Variant ret;
-        ret = arg;
-        return ret;
-    }
-
-    static void _register_methods()
-    {
-        register_method("method", &E621::method);
-        register_property<E621, String>("base/name", &E621::_name, String("E621"));
-        register_property<E621, int>("base/value", &E621::set_value, &E621::get_value, 0);
-    }
-
-    String _name;
-    int _value;
-
-    void set_value(int p_value)
-    {
-        _value = p_value;
-    }
-
-    int get_value() const
-    {
-        return _value;
-    }
-};
-
-extern "C"
+void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o)
 {
-    void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options* o)
-    {
-        Godot::gdnative_init(o);
-    }
+    godot::Godot::gdnative_terminate(o);
+}
 
-    void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options* o)
-    {
-        Godot::gdnative_terminate(o);
-    }
+void GDN_EXPORT godot_nativescript_init(void *handle)
+{
+    godot::Godot::nativescript_init(handle);
+    godot::register_class<E621>();
+}
 
-    void GDN_EXPORT godot_nativescript_init(void* handle)
-    {
-        Godot::nativescript_init(handle);
-        register_class<E621>();
-    }
+void E621::_register_methods()
+{
+}
+
+void E621::_init()
+{
+    Godot::print("Hello succas");
 }
---
+++ native/E621.hpp
@@ -0,0 +1,23 @@
+#ifndef E621_HPP
+#define E621_HPP
+
+#include <Godot.hpp>
+#include <Reference.hpp>
+
+extern "C"
+{
+    void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o);
+    void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_options *o);
+    void GDN_EXPORT godot_nativescript_init(void *handle);
+}
+
+class E621 : public godot::Reference
+{
+    GODOT_CLASS(E621, Reference);
+
+public:
+    static void _register_methods();
+    void _init();
+};
+
+#endif
